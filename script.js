9292d4bcb74bcffd1d5de9ef838690c77ad946bf
unzilashah <133483232+unzilashah@users.noreply.github.com>
2023-07-11 12:58:22 -0700

    Add files via upload
    add

---
+++ script.js
@@ -0,0 +1,44 @@
+// **********Normal function*********************
+  
+// function add (x,y){
+//     console.log()
+//     return x + y;
+// }
+//   add (10,5)
+// let add = (a,b) =>{
+//     return a + b 
+// }
+//  console.log(add(3,4))
+
+
+
+// let firstname= prompt('enter your first name')
+// let lastname = prompt('enter your last name')
+
+
+// function fullname(){
+//   console.log( 'hello',firstname,lastname)
+//   return firstname + lastname
+
+// }
+// fullname()
+
+
+
+
+// function getusername(c){
+//  let name = prompt('enter your full name!')
+//  c(name)
+// }
+
+// function printincn(ab){
+//   console.log('your name is :' + ab  )
+// }
+// function printinndw(ab){
+//   document.write('your name is : ' + ab)
+// }
+
+
+
+// getusername(printincn)
+// getusername(printinndw)
